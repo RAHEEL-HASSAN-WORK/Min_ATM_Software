7f0d79f91870e90ef5f839c54e88ce990e6dc090
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2025-07-15 12:57:51 +0500

    Initilize

---
+++ software.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cctype>
+#include<string>
+#include <conio.h>
+#include <fstream>
+using namespace std;
+
+string usernames[20] = {
+    "user1", "admin", "raheel", "guest", "alpha",
+    "beta", "gamma", "delta", "zara", "nashit",
+    "sana", "fahad", "aisha", "osama", "laiba",
+    "noor", "yasir", "hamza", "zain", "iqra"
+};
+
+string mpins[20];
+int size =20;
