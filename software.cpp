4e270d328ccd0fd23d26995407f1e68a3d80bde0
RAHEEL HASSAN <raheelhassanwork@gmail.com>
2025-07-16 12:39:29 +0500

    Add MPINS Function

--- software.cpp
+++ software.cpp
@@ -14,3 +14,19 @@ string usernames[20] = {
 
 string mpins[20];
 int size =20;
+
+void loadMPINs() {
+    ifstream infile("mpins.txt");
+    if (!infile) {
+        cout << "No MPIN file found. Using default values.\n";
+        string defaultMpins[20] = {
+            "1234", "1111", "4567", "9999", "2222",
+            "3333", "4444", "5555", "6666", "7777",
+            "8888", "1212", "3434", "5656", "7878",
+            "9090", "1010", "1313", "1515", "1717"
+        };
+        for (int i = 0; i < size; i++) {
+            mpins[i] = defaultMpins[i];
+        }
+        return;
+    }
